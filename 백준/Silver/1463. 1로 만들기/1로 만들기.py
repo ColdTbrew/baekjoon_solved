dc84d28ad4e93c15ee321ee0927d1e86ecf4f458
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2025-03-21 16:30:13 +0900

    [Silver III] Title: 1로 만들기, Time: 476 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,21 +1,15 @@
 import sys
 input = sys.stdin.readline
+n = int(input())
 
-x = int(input().strip())
+dp = [0]*(n+1)
 
-dp = [float('inf') for _ in range(10**6+1)]
-# print(dp)
-# dp[i] 는 숫자 i를 1로 만드는 데 필요한 최소 연산 횟수
-dp[1] = 0
-dp[2] = 1
-if x not in [1,2]:    
-    for i in range(3, x+1):
-        a = float('inf')
-        b = float('inf')
-        if i%2 == 0:
-            a = dp[i//2]+1
-        if i%3 == 0:
-            b = dp[i//3]+1
-        dp[i] = min(dp[i-1]+1, a, b)
+for i in range(2,n+1):
+    dp[i] = dp[i-1] + 1
+    if i % 2 == 0:
+        dp[i] = min(dp[i//2]+1, dp[i])
+    if i % 3 == 0:
+        dp[i] = min(dp[i//3]+1, dp[i]) 
 
-print(dp[x])
+
+print(dp[n])
