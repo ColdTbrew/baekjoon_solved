8f60df2c1ceeba13513b7ec0fa11f787c9ccd425
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2025-03-27 22:04:52 +0900

    [Gold III] Title: 웜홀, Time: 996 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -1,3 +1,4 @@
+
 from sys import stdin
 input = stdin.readline
 
@@ -13,17 +14,19 @@ def bf():
 TC = int(input())
 
 for _ in range(TC):
-      n, m, w = map(int, input().split())
-      edges = []
-      dist = [1e9] * (n + 1)
-      for i in range(m + w):
-            s, e, t = map(int, input().split())
-            if i >= m:
-                  t = -t
-            else:
-                  edges.append((e, s, t))
-            edges.append((s, e, t))
-      if bf():
-            print("YES")
-      else:
-            print("NO")
+    n, m, w = map(int, input().split())
+
+    edges = []
+
+    for _ in range(m):
+        s, e, t = map(int, input().split())
+        edges.append((s, e, t))
+        edges.append((e, s, t))
+    for _ in range(w):
+        s, e, t = map(int, input().split())
+        edges.append((s, e, -t))
+    dist = [1e9] * (n+1)
+    if bf():
+        print("YES")
+    else:
+        print("NO")
