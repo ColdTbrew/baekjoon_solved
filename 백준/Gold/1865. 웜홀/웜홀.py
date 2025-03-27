c1294076e60c3f1efa05b548c2b02a8053e0f04c
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2025-03-27 22:12:04 +0900

    [Gold III] Title: 웜홀, Time: 932 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -1,18 +1,7 @@
+import sys
+input = sys.stdin.readline
 
-from sys import stdin
-input = stdin.readline
-
-def bf():
-    for i in range(n):
-        for cur, next, cost in edges:
-            if dist[next] > dist[cur] + cost:
-                dist[next] = dist[cur] + cost
-                if i == n - 1:
-                    return True
-    return False
-                        
-TC = int(input())
-
+TC = int(input().strip())
 for _ in range(TC):
     n, m, w = map(int, input().split())
 
@@ -25,8 +14,22 @@ for _ in range(TC):
     for _ in range(w):
         s, e, t = map(int, input().split())
         edges.append((s, e, -t))
-    dist = [1e9] * (n+1)
-    if bf():
+    
+    def bellman_ford():
+        dists = [1e9] * (n+1)
+
+        for _ in range(n-1): # 벨만 포드 알고리즘은 n-1 번만 완화를 수행해도 최단거리가 다 구해짐
+            for s, e, t in edges:
+                if dists[s] + t < dists[e]:
+                    dists[e] = dists[s] + t
+
+            
+        for s, e, t in edges: # 하지만 한번 더 돌렸을때 갱신이 된다면 음수의 사이클이 존재한다는 의미
+            if dists[s] + t < dists[e]:
+                return True
+        return False
+    
+    if bellman_ford():
         print("YES")
     else:
         print("NO")
