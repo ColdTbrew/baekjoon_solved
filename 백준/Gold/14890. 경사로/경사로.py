419de1372beb4e964536b49418d536e0860f9b9a
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2025-04-10 22:08:23 +0900

    Update 경사로.py

--- 백준/Gold/14890. 경사로/경사로.py
+++ 백준/Gold/14890. 경사로/경사로.py
@@ -49,3 +49,53 @@ for j in range(N):
 
 # 결과 출력
 print(answer)
+
+
+import sys
+from collections import deque
+from itertools import combinations
+from itertools import permutations
+input = sys.stdin.readline
+
+N, L = map(int, input().split())
+
+maps = [list(map(int, input().split())) for _ in range(N)]
+
+def can_go(line):
+    visited = [False] * N
+    for i in range(N-1):
+        if line[i] == line[i+1]:
+            continue
+        diff = line[i] - line[i+1]
+
+        if abs(diff) != 1: # 높이차이가 1이 아니면 
+            return False
+        if diff == 1: # 차이가 양수면 왼쪽이 높다는거니까 L의 거리만큼 바닥의 높이가 같다면 경사로를 놓을 수 있다
+            if i + L >= N:
+                return False
+            for j in range(i + 1, i + 1+L): # 오른쪽 칸인 i+1부터 i+1 + L 까지
+                if line[j] != line[i+1] or visited[j]: # 높이가 같지 않거나 방문햇다면 false
+                    return False
+                visited[j] = True # 경사로 설치
+        elif diff == -1:
+            if i - L + 1 < 0:
+                return False
+            for j in range(i-L+1, i+1):
+                if line[j] != line[i] or visited[j]:
+                    return False
+                visited[j] = True
+    return True
+
+answer = 0
+for i in range(N):
+    if can_go(maps[i]):
+        answer += 1
+
+for j in range(N):
+    col = [maps[i][j] for i in range(N)]
+    if can_go(col):
+        answer += 1
+
+print(answer)
+# 가로 검사
+# 세로 검사
