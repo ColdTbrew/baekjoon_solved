1108f50a7a9038accf6fff9ab444b5c46b79cb8b
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2025-04-18 16:22:27 +0900

    Update 이중 우선순위 큐.py

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 import heapq
 
 input = sys.stdin.readline
@@ -9,45 +8,61 @@ for _ in range(T):
     k = int(input().strip())
     min_q = []
     max_q = []
-    valid = {}  # 값의 유효성 추적: {값: 개수}
+    valid = {}  # {값: 개수}로 유효성 추적
     
-    for i in range(k):
+    for _ in range(k):
         cmd, num = input().split()
         num = int(num)
         
         if cmd == 'I':
-            # 삽입: min_q와 max_q에 추가, valid 업데이트
-            heapq.heappush(min_q, (num, i))  # (값, 삽입 순서)
-            heapq.heappush(max_q, (-num, i))  # (-값, 삽입 순서)
-            valid[num] = valid.get(num, 0) + 1
+            # 삽입: 값만 힙에 추가
+            heapq.heappush(min_q, num)
+            heapq.heappush(max_q, -num)
+            if num in valid:
+                valid[num] += 1
+            else:
+                valid[num] = 1
             
         elif cmd == 'D' and valid:
             if num == 1:  # 최대값 삭제
-                # max_q에서 유효한 최대값 찾기
-                while max_q and valid.get(-max_q[0][0], 0) == 0:
+                # 유효한 최대값 찾기
+                while max_q:
+                    val = -max_q[0]
+                    if val in valid and valid[val] > 0:
+                        break
                     heapq.heappop(max_q)
                 if max_q:
-                    val, _ = max_q[0]
-                    valid[-val] -= 1
+                    valid[val] -= 1
+                    if valid[val] == 0:
+                        del valid[val]
                     heapq.heappop(max_q)
             elif num == -1:  # 최소값 삭제
-                # min_q에서 유효한 최소값 찾기
-                while min_q and valid.get(min_q[0][0], 0) == 0:
+                # 유효한 최소값 찾기
+                while min_q:
+                    val = min_q[0]
+                    if val in valid and valid[val] > 0:
+                        break
                     heapq.heappop(min_q)
                 if min_q:
-                    val, _ = min_q[0]
                     valid[val] -= 1
+                    if valid[val] == 0:
+                        del valid[val]
                     heapq.heappop(min_q)
     
-    # 최종 결과 계산: 유효한 값만 추출
-    while min_q and valid.get(min_q[0][0], 0) == 0:
+    # 유효한 값만 남기기
+    while min_q:
+        val = min_q[0]
+        if val in valid and valid[val] > 0:
+            break
         heapq.heappop(min_q)
-    while max_q and valid.get(-max_q[0][0], 0) == 0:
+    while max_q:
+        val = -max_q[0]
+        if val in valid and valid[val] > 0:
+            break
         heapq.heappop(max_q)
     
+    # 결과 출력
     if min_q and max_q:
-        max_val = -max_q[0][0]
-        min_val = min_q[0][0]
-        print(max_val, min_val)
+        print(-max_q[0], min_q[0])
     else:
         print("EMPTY")
