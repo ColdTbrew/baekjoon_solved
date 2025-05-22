0a72ef879ea678fa214e4214741f135a52a5e0ad
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2025-05-22 18:54:04 +0900

    [Silver III] Title: 피보나치 함수, Time: 124 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,21 +1,16 @@
-import sys
-input = sys.stdin.readline
+# 피보나치 출력 횟수 계산
+T = int(input())
+# DP 테이블: dp[i][0]은 0의 횟수, dp[i][1]은 1의 횟수
+dp = [[0, 0] for _ in range(41)]
+dp[0] = [1, 0]  # N=0: 0이 1번, 1이 0번
+dp[1] = [0, 1]  # N=1: 0이 0번, 1이 1번
 
-# 메모이제이션 테이블: (fib_value, zero_count, one_count) 저장
-memo = [None] * 41
-memo[0] = (0, 1, 0)  # fib(0): 값=0, zero=1, one=0
-memo[1] = (1, 0, 1)  # fib(1): 값=1, zero=0, one=1
+# DP로 0과 1의 출력 횟수 미리 계산
+for i in range(2, 41):
+    dp[i][0] = dp[i-1][0] + dp[i-2][0]  # 0의 횟수 = 이전 두 단계의 0 횟수 합
+    dp[i][1] = dp[i-1][1] + dp[i-2][1]  # 1의 횟수 = 이전 두 단계의 1 횟수 합
 
-def fibonacci(n):
-    if memo[n] is not None:
-        return memo[n]
-    val1, zero1, one1 = fibonacci(n-1)
-    val2, zero2, one2 = fibonacci(n-2)
-    memo[n] = (val1 + val2, zero1 + zero2, one1 + one2)
-    return memo[n]
-
-n = int(input().strip())
-for _ in range(n):
-    F = int(input().strip())
-    _, zero, one = fibonacci(F)  # 값, zero, one 반환
-    print(zero, one)
+# 테스트 케이스 처리
+for _ in range(T):
+    N = int(input())
+    print(dp[N][0], dp[N][1])
