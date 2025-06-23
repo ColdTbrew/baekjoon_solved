791aa7fedc44c2fd63adf10e2d6d04ae83460021
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2025-06-23 23:33:42 +0900

    [Bronze V] Title: 사칙연산, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,9 @@
+# 두 자연수 A와 B 입력받기
+A, B = map(int, input().split())
+
+# 사칙연산 결과 출력
+print(A + B)  # 첫째 줄: 덧셈
+print(A - B)  # 둘째 줄: 뺄셈
+print(A * B)  # 셋째 줄: 곱셈
+print(A // B) # 넷째 줄: 몫 (정수 나눗셈)
+print(A % B)  # 다섯째 줄: 나머지
