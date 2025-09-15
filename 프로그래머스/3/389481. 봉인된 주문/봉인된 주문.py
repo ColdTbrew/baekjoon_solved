bbe69d7f4e31512dd84e793cb37b8fde9bd93a65
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2025-09-15 10:12:37 +0900

    [level 3] Title: 봉인된 주문, Time: 1630.37 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.py
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.py
@@ -2,19 +2,26 @@ import math
 def solution(n, bans):
     answer = ''
     table = {}
-    for idx, value in enumerate(list("abcdefghijklmnopqrstuvwxyz"), 1):
+    for idx, value in enumerate(list("abcdefghijklmnopqrstuvwxyz"), 1): # 인덱스를 1부터 생성해라
         table[value] = idx
-    bans = sorted(bans, key = lambda x: (len(x), x))
+    # print(table)
+    bans = sorted(bans, key = lambda x: (len(x), x)) # 길이로 먼저 정렬, 그다음 알파벳 순서로
+    # print(bans)
     
-    actual_number = n
+    actual_number = n # 찾아야할 숫자의 번호
     for string in bans:
+        # 현재 문자열 'string'을 뒤집어서 리스트로 만듭니다. (26진법 계산을 용이하게 하기 위함)
         string_list = list(reversed(string))
         number = 0
+        # 문자열의 길이만큼 반복하며, 각 자릿수(idx)에 대한 값을 계산합니다.
         for idx in range(len(string)):
+            # 26의 거듭제곱에 해당 알파벳의 번호를 곱하여 10진수 값을 계산하고 'number'에 더합니다.
+            # print(string_list[idx])
             number += (math.pow(26, idx) * table[string_list[idx]])
-        if number > actual_number:
+        if number > actual_number: # 계산된 10진수 값이 actual_number 보다 크면 다음 문자열을 확인할 필요가 없음
             break
         actual_number += 1
+        
     n_26 = []
     while actual_number> 0:
         share = actual_number // 26
@@ -24,7 +31,7 @@ def solution(n, bans):
             share -= 1
         n_26.append(remainer)
         actual_number = share
-
+    # print(n_26)
     n_26.reverse()
     alphabets = list("abcdefghijklmnopqrstuvwxyz")
     answer = []
