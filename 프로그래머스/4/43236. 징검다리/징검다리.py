b9cc98e42f5cf7fdaaa30a660cde3022092369e8
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2025-09-24 18:41:22 +0900

    [level 4] Title: 징검다리, Time: 173.62 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -14,9 +14,13 @@ def solution(distance, rocks, n):
                 removed += 1
             else:
                 prev = rocks[idx] 
-        if removed <= n:
+        if removed <= n:#가능 케이스, 제거한 바위 수가 n 이하 → mid가 가능하므로 더 큰 값 탐색 (low = mid + 1).
             low = mid + 1
-        else:
+        else: #불가능 케이스, 제거한 바위 수가 n 초과 → mid가 너무 크므로 범위 축소 (high = mid - 1).
             high = mid - 1
-    
+        print("try mid ", mid)
+        print("low", low)
+        print("high", high)
     return high
+# 탐색이 끝날 때, high는 조건을 만족하는 최대 mid 값(즉, 최대화된 최소 거리)을 가리킴.
+# low는 high + 1이 되어 조건을 만족하지 않는 첫 번째 값(너무 큰 최소 거리)을 가리킴.
